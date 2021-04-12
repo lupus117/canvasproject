b7088c37a86804fd5f0251967433945c176d2497
lupus117 <44517641+lupus117@users.noreply.github.com>
2021-04-12 13:50:10 +0300

    workbox test

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,23 +1,7 @@
-const staticDevCoffee = "dev-coffee-site-v1";
-const assets = [
-    "/",
-    "/index.html",
-    "/main.css",
-    "/main.js",
-    "/images/Lupus.png",
-];
-self.addEventListener("install", (installEvent) => {
-    installEvent.waitUntil(
-        caches.open(staticDevCoffee).then((cache) => {
-            cache.addAll(assets);
-        })
-    );
-});
-
-self.addEventListener("fetch", (fetchEvent) => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then((res) => {
-            return res || fetch(fetchEvent.request);
-        })
-    );
-});
+importScripts(
+    "https://storage.googleapis.com/workbox-cdn/releases/6.1.1/workbox-sw.js"
+);
+workbox.routing.registerRoute(
+    ({ request }) => request.destination === "image",
+    new workbox.strategies.CacheFirst()
+);
