da57730a4d58ebf8ad35b7e02c5418a57044de6e
lupus117 <44517641+lupus117@users.noreply.github.com>
2021-04-12 16:02:44 +0300

    switch to networkfirst

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,5 +3,5 @@ importScripts(
 );
 workbox.routing.registerRoute(
     ({ request }) => request.destination === "image",
-    new workbox.strategies.CacheFirst()
+    new workbox.strategies.NetworkFirst()
 );
